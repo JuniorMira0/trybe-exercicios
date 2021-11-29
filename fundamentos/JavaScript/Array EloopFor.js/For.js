e2463555ba524d6efd902c6bb44251eac361643c
JuniorMira <j-unyor14@hotmail.com>
2021-11-29 17:51:17 -0300

    atividades js

---
+++ fundamentos/JavaScript/Array EloopFor.js/For.js
@@ -0,0 +1,12 @@
+/* let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (index = 0; index <= groceryList.length; index += 1) {
+    console.log(groceryList[index])
+} */
+
+//
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for(let nomes of names){
+    console.log(nomes)
+}
