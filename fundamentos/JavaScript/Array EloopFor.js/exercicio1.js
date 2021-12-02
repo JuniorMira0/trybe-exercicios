04eb2c15355f8ca0ad0c49a4be5b65f9c9a60f1e
JuniorMira <j-unyor14@hotmail.com>
2021-12-02 11:22:46 -0300

    att de atividades

--- fundamentos/JavaScript/Array EloopFor.js/exercicio1.js
+++ fundamentos/JavaScript/Array EloopFor.js/exercicio1.js
@@ -1,12 +1,11 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
 
-for (index = 0; index < numbers.length; index += 1){
-    result += numbers[index]
+for (let index = 0; index < fruits.length; index += 1) {
+  sum += fruits[index]
 }
-let average = result / numbers.length
- if (average > 20) {
-     console.log("valor maior que 20")
- } else {
-     console.log("valor menor ou igual a 20")
+if (sum > 15) {
+  console.log(sum)
+} else {
+  console.log('Valor menor que 15')
 }
---
+++ fundamentos/JavaScript/Array EloopFor.js/logicaDeProgramação/Exercicio1.js
@@ -0,0 +1,8 @@
+let n = 5;
+let symbol = '*';
+let imputline = '';
+
+for (let index = 0; index <= n; index += 1){
+    imputline += symbol
+    console.log(imputline)
+}
